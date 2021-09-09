77682dbdc4b663cebdedacc65e36421f13c19767
Salokya Kumar <kumar.salokya007@gmail.com>
2021-09-09 08:12:51 +0530

    added editor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, {Component} from "react";
+import './App.css'
 import firebase from "firebase";
+import EditorComponent from "./editor/editor";
+import SidebarComponent from "./sidebar/sidebar";
+import NavbarComponent from "./navbar/navbar"
+import {Container,Row,Col} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {ClimbingBoxLoader} from 'react-spinners'
+import styles from './styles';
+import {withStyles} from "@material-ui/core";
 
 class App extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            selectedNoteIndex:null,
+            selectedNote:null,
+            notes:null,
+            user:"",
+            loading:true
+        };
+    }
+
+    componentWillMount() {
+        if(this.timeHanlerv){
+            clearTimeout(this.timeHanlerv);
+            this.timeHanlerv=0;
+        }
+    }
 
     render(){
 
@@ -9,12 +36,40 @@ class App extends Component{
 
         return(
             <div>
+                {this.state.loading ?
+                    <div>
+                        <h3 className={classes.loader1}>backSlash</h3>
+                        <ClimbingBoxLoader
+                            color={"#000"}
+                            speedMultiplier="1.2"
+                            size={30}
+                            loading={this.state.loading}
+                        />
+                        <p className={classes.loader2}>An online Text Editor + Notes App.</p>
+                    </div>
+                    :
                     <div className="app-container">
                         <NavbarComponent user={this.state.user}/>
                         <Row className="row-container">
                             <Col xs={12} md={4} lg={3} className="side-item">
                                 <SidebarComponent
+                                    selectedNoteIndex={this.state.selectedNoteIndex}
+                                    notes={this.state.notes}
+                                    deleteNote={this.deleteNote}
+                                    selectNote={this.selectNote}
+                                    newNote={this.newNote}
+                                    user={this.state.user}
                                 />
+                            </Col>
+                            <Col xs={12} md={8} lg={9}>
+                                {this.state.selectedNote ? (
+                                    <EditorComponent
+                                        selectedNote={this.state.selectedNote}
+                                        selectedNoteIndex={this.state.selectedNoteIndex}
+                                        notes={this.state.notes}
+                                        noteUpdate={this.noteUpdate}
+                                    />
+                                ) : null}
                             </Col>
                         </Row>
                     </div>
@@ -23,6 +78,110 @@ class App extends Component{
         )
     }
 
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(async user => {
+            if (!user) {
+                this.props.history.push("/login");
+            }
+            else{
+                firebase
+                    .firestore()
+                    .collection('notes')
+                    .where("user", "==", firebase.auth().currentUser.email)
+                    //automatically get called when collection is updated
+                    .onSnapshot(async serverUpdate =>{
+                        const notes = serverUpdate.docs.map(_doc =>{
+                            //grabs data from doc
+                            const  data =_doc.data();
+                            data['id'] = _doc.id;
+                            return data;
+                        });
+                        await this.setState({
+                            notes:notes,
+                            user:firebase.auth().currentUser.email
+                        });
+                    });
+                {
+                    this.timeHanlerv = setTimeout(() => {
+                        this.setState({loading: false});
+                    }, 4000)
+                }
+            }
+        });
+    };
 
+    selectNote = (note,index) => {
+        this.setState({
+            selectedNoteIndex: index,
+            selectedNote:note
+        })
+    }
+
+    noteUpdate = (id,noteObject) =>{
+        firebase
+            .firestore()
+            .collection('notes')
+            .doc(id)
+            .update({
+               title:noteObject.title,
+               body:noteObject.body,
+               timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+               user: firebase.auth().currentUser.email
+            });
+    }
+
+    newNote = async (title) => {
+        const note = {
+            title:title,
+            body:''
+        };
+        const newFromDB = await firebase
+            .firestore()
+            .collection('notes')
+            .add({
+                title: note.title,
+                body:note.body,
+                timestamp:firebase.firestore.FieldValue.serverTimestamp(),
+                user: firebase.auth().currentUser.email
+            });
+        const newID = newFromDB.id;
+        await this.setState({
+            notes:[...this.state.notes,note]
+        })
+        const newNoteIndex = this.state.notes.indexOf(this.state.notes.filter(_note => _note.id === newID)[0]);
+        this.setState({
+            selectedNote:this.state.notes[newNoteIndex],
+            selectedNoteIndex:newNoteIndex
+        })
+    }
+
+    deleteNote = async  (note) => {
+        const noteIndex = this.state.notes.indexOf(note);
+        await this.setState({notes:this.state.notes.filter(_note => _note !== note)})
+        if(this.state.selectedNoteIndex === noteIndex){
+            this.setState({
+                selectedNoteIndex:null,
+                selectedNote:null
+            })
+        }
+        else{
+            this.state.notes.length > 1 ?
+                this.selectNote(this.state.notes[this.state.selectedNoteIndex - 1],this.state.selectedNoteIndex - 1) :
+                this.setState({
+                    selectedNoteIndex:null,
+                    selectedNote:null
+                })
+        }
+        firebase
+            .firestore()
+            .collection('notes')
+            .doc(note.id)
+            .delete();
+    }
+
+    signOut = () => {
+        firebase.auth().signOut();
+    };
+}
 
 export default withStyles(styles)(App);
---
+++ src/editor/editor.js
@@ -0,0 +1,144 @@
+import React, {Component} from 'react';
+import ReactQuill from 'react-quill';
+import debounce from '../helpers';
+import BorderColorIcon from '@material-ui/icons/BorderColor';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import Typography from "@material-ui/core/Typography";
+
+class EditorComponent extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            text:'',
+            title:'',
+            id:'',
+            update:''
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({
+            text:this.props.selectedNote.body,
+            title:this.props.selectedNote.title,
+            id:this.props.selectedNote.id,
+            update: this.props.selectedNote.timestamp
+                ? this.props.selectedNote.timestamp.toDate()
+                : new Date()
+        });
+    }
+
+    componentDidUpdate = () => {
+        if(this.props.selectedNote.id !== this.state.id){
+            this.setState({
+                text:this.props.selectedNote.body,
+                title:this.props.selectedNote.title,
+                id:this.props.selectedNote.id,
+                update: this.props.selectedNote.timestamp
+                    ? this.props.selectedNote.timestamp.toDate()
+                    : new Date()
+            });
+        }
+    };
+
+    render(){
+
+        const { classes } = this.props;
+
+        return (
+            <div className={classes.editorContainer}>
+                <div className={classes.editorNavbar}>
+                    <div className={classes.inputContainer}>
+                        <BorderColorIcon className={classes.borderIcon}/>
+                        <input
+                            className={classes.titleInput}
+                            placeholder="Note title..."
+                            value={this.state.title ? this.state.title : ""}
+                            onChange={e => this.updateTitle(e.target.value)}
+                        />
+                    </div>
+                    <Typography
+                        value={this.state.update}
+                        className={classes.upDate}
+                        variant="body2"
+                        onChange={e => this.updateDate(e.target.value)}
+                    >
+                        Last update: {this.state.update.toString()}
+                    </Typography>
+                </div>
+                <ReactQuill
+                    value={this.state.text}
+                    onChange={this.updateBody}
+                    modules={EditorComponent.modules}
+                    formats={EditorComponent.formats}
+                />
+            </div>
+        );
+    }
+
+
+    updateBody = async (val) =>{
+        await this.setState({text:val});
+        this.update();
+    };
+
+    updateDate = async val => {
+        await this.setState({ update: val });
+        this.update();
+    };
+
+    updateTitle = async (txt) => {
+        await this.setState({
+            title:txt
+        });
+        this.update();
+    }
+
+    update = debounce(() =>{
+        this.props.noteUpdate(this.state.id,{
+            title:this.state.title,
+            body:this.state.text,
+            update: this.state.update
+        })
+    },1500);
+}
+
+EditorComponent.modules = {
+    toolbar: [
+        [{ header: "1" }, { header: "2" }, { font: [] }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote", "code-block"],
+        [
+            { list: "ordered" },
+            { list: "bullet" },
+            { indent: "-1" },
+            { indent: "+1" }
+        ],
+        ["link", "image", "video"],
+        ["clean"]
+    ],
+    clipboard: {
+        matchVisual: false
+    }
+};
+
+
+EditorComponent.formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+    "code-block"
+];
+
+export default withStyles(styles)(EditorComponent);
