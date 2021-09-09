d650825a060eb898f4960a52ad7ffe64a9d69767
Salokya Kumar <kumar.salokya007@gmail.com>
2021-09-09 09:59:04 +0530

    added editor styles

---
+++ src/editor/styles.js
@@ -0,0 +1,57 @@
+const styles = theme => ({
+    editorNavbar: {
+        marginTop: '1%',
+        display: "flex",
+        backgroundColor: "#29487d",
+        color: "white",
+        flexDirection: 'column'
+    },
+    borderIcon:{
+        marginLeft:'1%',
+        "@media only screen and (max-width:768px)":{
+            display:'none'
+        }
+    },
+    titleInput: {
+        marginRight: "0",
+        border: "none",
+        fontSize: "20px",
+        backgroundColor: "#29487d",
+        borderStyle:'solid',
+        borderWidth:'2px',
+        borderColor:'#fff',
+        color: "white",
+        marginLeft: '2%',
+        paddingLeft: "10px",
+        "@media only screen and (max-width:768px)":{
+            width:'90%',
+            marginTop:'1%',
+            marginBottom:'2%',
+            marginLeft:'0'
+        }
+    },
+    editorContainer: {
+        height:'70vh'
+    },
+    upDate: {
+        flexWrap: "wrap",
+        paddingRight: "10px",
+        alignSelf: "flex-end",
+        "@media only screen and (max-width: 768px)": {
+            textAlign: "center",
+            fontSize: '11px',
+            marginBottom: '1%',
+        }
+    },
+    inputContainer: {
+        display: "flex",
+        alignItems: "center",
+        marginTop: "5px",
+        marginLeft: "5px",
+        "@media only screen and (max-width: 768px)": {
+            flexDirection: "column"
+        }
+    }
+});
+
+export default styles;
