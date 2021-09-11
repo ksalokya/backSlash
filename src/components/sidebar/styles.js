c261a50422192ffa1fd0f9dd6d75fe48e787d099
Salokya Kumar <kumar.salokya007@gmail.com>
2021-09-11 13:23:27 +0530

    added sidebar styles

---
+++ src/components/sidebar/styles.js
@@ -0,0 +1,57 @@
+const styles = theme => ({
+    root: {
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: "0px 0px 2px black"
+    },
+    newChatBtn: {
+        borderRadius: '0px'
+    },
+    newNoteBtn: {
+        width: '100%',
+        height: '8.5%',
+        borderBottom: '1px solid black',
+        borderRadius: '10px',
+        backgroundColor: '#29487d',
+        marginBottom:'10px',
+        color: 'white',
+        '&:hover': {
+            backgroundColor: '#88a2ce'
+        }
+    },
+    sidebarContainer: {
+        marginTop: '3%',
+        width:'100%',
+        height: '88vh',
+        paddingLeft: "1%",
+        paddingRight: "1%",
+        overflowY: 'scroll',
+        overflowX: 'hidden',
+        "@media only screen and (max-width: 768px)": {
+            height:'initial',
+            paddingLeft: "3%",
+            paddingRight: '3%'
+        }
+    },
+    newNoteInput: {
+        marginBottom: '5px',
+        height: '35px',
+        width:'100%',
+        outline: 'none',
+        border: 'none',
+        paddingLeft: '5px',
+        '&:focus': {
+            outline: '2px solid rgba(81, 203, 238, 1)'
+        }
+    },
+    newNoteSubmitBtn: {
+        width: '100%',
+        backgroundColor: '#28787c',
+        borderRadius: '0px',
+        color: 'white',
+        '&:hover' :{
+            backgroundColor:'#2596be'
+        }
+    }
+});
+
+export default styles;
