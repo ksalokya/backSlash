e7a72d0b780dd0388347c20e947471c6d18b3c7e
Salokya Kumar <kumar.salokya007@gmail.com>
2021-09-11 13:23:08 +0530

    added sidebar

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactQuill from 'react-quill';
-import debounce from '../helpers';
+import debounce from '../helper/helpers';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
