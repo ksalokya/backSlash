e67e5eab3668a27e5e864727d72b84b458b55f59
Salokya Kumar <kumar.salokya007@gmail.com>
2022-05-02 21:40:00 +0530

    misc changes

--- src/index.js
+++ src/index.js
@@ -9,14 +9,24 @@ import Instruction from "./components/instruction/Instruction";
 import VerifyEmail from "./components/verifyemail/VerifyEmail";
 import ResetPassword from "./components/resetpassword/ResetPassword";
 
+// firebase.initializeApp({
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID,
+//     measurementId: process.env.REACT_APP_MEASUREMENT_ID
+// });
+
 firebase.initializeApp({
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyCrwtzO7_lqasLgRAJrkpZYBJ9IIXsE1ps",
+    authDomain: "backslash-1ebbc.firebaseapp.com",
+    projectId: "backslash-1ebbc",
+    storageBucket: "backslash-1ebbc.appspot.com",
+    messagingSenderId: "983579998637",
+    appId: "1:983579998637:web:e6f39b3be6ecf7a7b7c924",
+    measurementId: "G-KBL6QNZ3S2"
 });
 
 firebase.analytics();
